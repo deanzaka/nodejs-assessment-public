33b01bfa99927a194486d5670b88eb64045ac2a8
deanzaka <hdeanzak@id.ibm.com>
2020-11-15 20:47:20 +0700

    add basic function test

--- main.js
+++ main.js
@@ -2,4 +2,25 @@
 
 exports.sumFunction = (a, b) => {
   // TODO: return a + b
+  return null;
+};
+
+exports.countWord = (text, word) => {
+  // TODO: return the count of word occurence in the text (case insensitive)
+  return null;
+};
+
+exports.findMissingNumber = (arrayNumber) => {
+  // TODO: return the missing number in the array of 1 to 100
+  return null;
+};
+
+exports.findDuplicates = (arrayNumber) => {
+  // TODO: return array of duplicates number
+  return null;
+};
+
+exports.findIncorrects = (ref, arrayWord) => {
+  // TODO: return incorrect NATO phonetic alpabhet
+  return null;
 };
--- test/test.js
+++ test/test.js
@@ -6,14 +6,93 @@ const generateNumber = (low, high) => {
   return Math.floor(Math.random() * (high - low + 1) + low);
 };
 
-describe("Basic functions:", function () {
-  describe("Sum function", function () {
-    it("should return the sum of variable a and b", function () {
+describe("Basic functions:", () => {
+  describe.skip("Add function", () => {
+    it("should return the sum of variable a and b", () => {
       const a = generateNumber(0, 100);
       const b = generateNumber(0, 100);
       const result = a + b;
 
       assert.equal(result, main.sumFunction(a, b));
+    });
+  });
+
+  describe.skip("Count word function", () => {
+    it("should count the occurence of given text (case insensitive)", () => {
+      const text = "This is the test string for this test task";
+
+      assert.equal(main.countWord(text, "this"), 2);
+      assert.equal(main.countWord(text, "TEST"), 2);
+      assert.equal(main.countWord(text, "is"), 1);
+    });
+  });
+
+  describe.skip("Find missing number function", () => {
+    it("should find missing number in array of 1 - 100", () => {
+      const rand = generateNumber(1, 100);
+      let numArray = [];
+      for (let i = 1; i <= 100; i++) {
+        if (i != rand) numArray.push(i);
+      }
+
+      assert.equal(main.findMissingNumber(numArray), rand);
+    });
+  });
+
+  describe.skip("Find duplicates function", () => {
+    it("should return an array of duplicates number (sorted)", () => {
+      const a = [0, 1, 1, 2, 3, 4, 4, 5, 6, 7, 8, 8, 9];
+      const b = [9, 9, 9, 8, 7, 6, 5, 4, 3, 2, 2, 1, 1, 0];
+      const c = [0, 9, 4, 5, 7, 2, 4, 5, 7, 8, 1, 2, 3, 6];
+
+      assert.deepEqual(main.findDuplicates(a), [1, 4, 8]);
+      assert.deepEqual(main.findDuplicates(b), [1, 2, 9]);
+      assert.deepEqual(main.findDuplicates(c), [2, 4, 5, 7]);
+    });
+  });
+
+  describe("Find incorrect phonetic function", () => {
+    it("should return array of incorrect NATO phonetic alphabet", () => {
+      const ref = [
+        "Alfa",
+        "Bravo",
+        "Charlie",
+        "Delta",
+        "Echo",
+        "Foxtrot",
+        "Golf",
+        "Hotel",
+        "India",
+        "Juliett",
+        "Kilo",
+        "Lima",
+        "Mike",
+        "November",
+        "Oscar",
+        "Papa",
+        "Quebec",
+        "Romeo",
+        "Sierra",
+        "Tango",
+        "Uniform",
+        "Victor",
+        "Whiskey",
+        "X-ray",
+        "Yankee",
+        "Zulu",
+      ];
+
+      const a = ["India", "Bravo", "Mama", "Juliet", "Tango", "India"];
+      const b = ["Whiskey", "Alpha", "Tango", "Siera", "Octa", "Nopember"];
+
+      firstTest = main.findIncorrects(ref, a);
+      for (let item of firstTest) {
+        assert.equal(ref.includes(item), false);
+      }
+      secondTest = main.findIncorrects(ref, b);
+      for (let item of secondTest) {
+        assert.equal(ref.includes(item), false);
+      }
     });
   });
 });
